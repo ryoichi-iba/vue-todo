4b311ba912f312bc2ea7b85d64f769248d27aa93
Ryoichi Iba <ryoichiaz18@gmail.com>
2021-02-26 17:26:15 +0900

    fxi item controller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -78,7 +78,7 @@ class ItemController extends Controller
 
         if ($existingItem) {
             $existingItem->completed = $request->item['completed'] ? true : false;
-            $existingItem->completed_at = $request->item['completed_at'] ? Carbon::now() : false;
+            $existingItem->completed_at = $request->item['completed'] ? Carbon::now() : false;
             $existingItem->save();
             return $existingItem;
         }
